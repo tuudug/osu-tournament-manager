ce10e154039c54f9a84b0df628bd771cf02d0c87
tuudug <tuudugprogrammer@gmail.com>
2024-12-26 02:29:30 +0800

    Fix auth build error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,5 @@
-import OsuProvider from "@/app/lib/auth/OsuProvider";
-import NextAuth, { Session, AuthOptions } from "next-auth";
-import { JWT } from "next-auth/jwt";
-
-export const authOptions: AuthOptions = {
-  providers: [
-    OsuProvider({
-      clientId: process.env.OSU_CLIENT_ID || "",
-      clientSecret: process.env.OSU_CLIENT_SECRET || "",
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: Session; token: JWT }) {
-      session.accessToken = token.accessToken as string;
-      return session;
-    },
-  },
-};
+import { authOptions } from "@/utils/auth-options";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
---
+++ utils/auth-options.ts
@@ -0,0 +1,24 @@
+import OsuProvider from "@/app/lib/auth/OsuProvider";
+import { AuthOptions, Session } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    OsuProvider({
+      clientId: process.env.OSU_CLIENT_ID || "",
+      clientSecret: process.env.OSU_CLIENT_SECRET || "",
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }: { session: Session; token: JWT }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
+};
