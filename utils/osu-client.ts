c8f96102e427ac016079b3ce34e847d466e8ddc3
tuudug <tuudugprogrammer@gmail.com>
2024-12-26 12:25:46 +0800

    Modify osu-client to not be persistent between users

--- utils/osu-client.ts
+++ utils/osu-client.ts
@@ -1,14 +1,10 @@
 import * as osu from "osu-api-v2-js";
 
-let apiInstance: osu.API | null = null;
-
 interface ClientProps {
   token?: string;
 }
 
 export async function getOsuClient({ token }: ClientProps) {
-  if (apiInstance) return apiInstance;
-
   const clientId = process.env.OSU_CLIENT_ID;
   const clientSecret = process.env.OSU_CLIENT_SECRET;
 
@@ -16,7 +12,7 @@ export async function getOsuClient({ token }: ClientProps) {
     throw new Error("Missing osu! client ID or secret");
   }
 
-  apiInstance = await osu.API.createAsync(Number(clientId), clientSecret);
+  let apiInstance = await osu.API.createAsync(Number(clientId), clientSecret);
 
   if (token) {
     apiInstance.access_token = token;
