2924de082566847895b60f9cadd658d7e15d0d9a
tuudug <tuudugprogrammer@gmail.com>
2025-02-14 21:11:36 +0800

    OAuth Token Expired session clear

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,6 +2,6 @@ import "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    accessToken: string;
+    accessToken?: string;
   }
 }
--- utils/auth/auth-options.ts
+++ utils/auth/auth-options.ts
@@ -1,6 +1,7 @@
 import OsuProvider from "@/services/osu/osu-provider";
 import { AuthOptions, Session } from "next-auth";
 import { JWT } from "next-auth/jwt";
+import { jwtDecode } from "jwt-decode";
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -17,7 +18,24 @@ export const authOptions: AuthOptions = {
       return token;
     },
     async session({ session, token }: { session: Session; token: JWT }) {
-      session.accessToken = token.accessToken as string;
+      if (token.accessToken) {
+        try {
+          const decodedToken: { exp: number } = jwtDecode(
+            token.accessToken as string,
+          );
+          if (decodedToken.exp * 1000 < Date.now()) {
+            // Token is expired, clear the session
+            session.accessToken = undefined;
+            return session;
+          }
+        } catch (error) {
+          // Handle token decoding errors (e.g., invalid token format)
+          console.error("Error decoding access token:", error);
+          session.accessToken = undefined;
+          return session; // Clear the session if decoding fails
+        }
+        session.accessToken = token.accessToken as string;
+      }
       return session;
     },
   },
